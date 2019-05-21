e6bb32d91d9ca0b519c93fcd30122a913290c8c5
Jiri Dostal <jdostal@redhat.com>
2019-05-21 14:57:08 +0200

    Fix postprocess script

--- postProcess.sh
+++ postProcess.sh
@@ -6,5 +6,5 @@ sed 's/\s*$//' -i $1
 
 # https://github.com/OpenAPITools/openapi-generator/issues/2154
 sed 's/OneOfResolutionsboolean/Resolutions \| boolean/' -i $1
-
+sed 's/OneOf[a-zA-Z]*/Object/' -i $1
 # generator warnings https://github.com/OpenAPITools/openapi-generator/issues/1714
