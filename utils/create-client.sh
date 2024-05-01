9de7796b7b3182c5e013ca68ce40d540d88416b9
Austin Pinkerton <apinkert@redhat.com>
2024-05-01 11:35:54 -0400

    chore: support OSX and Linux create-client

--- utils/create-client.sh
+++ utils/create-client.sh
@@ -6,6 +6,15 @@ read client
 mkdir packages/$client
 rsync -am utils/nx-base/ packages/$client
 
-sed -i '' "s/CLIENTNAME/$client/g" packages/$client/package.json
-sed -i '' "s/CLIENTNAME/$client/g" packages/$client/project.json
-sed -i '' "s/CLIENTNAME/$client/g" packages/$client/jest.config.ts
+# https://unix.stackexchange.com/questions/13711/differences-between-sed-on-mac-osx-and-other-standard-sed
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  sed -i '' "s/CLIENTNAME/$client/g" packages/$client/package.json
+  sed -i '' "s/CLIENTNAME/$client/g" packages/$client/project.json
+  sed -i '' "s/CLIENTNAME/$client/g" packages/$client/jest.config.ts
+
+else
+  sed "s/CLIENTNAME/$client/g" packages/$client/package.json -i
+  sed "s/CLIENTNAME/$client/g" packages/$client/project.json -i
+  sed "s/CLIENTNAME/$client/g" packages/$client/jest.config.ts -i
+
+fi
