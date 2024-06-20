898f1b8ee4f7589b915ca6ad958bccd407000058
Austin Pinkerton <apinkert@redhat.com>
2024-06-20 13:59:20 -0400

    feat: add inital support for multiple API versions for rbac

--- packages/rbac/index.ts
+++ packages/rbac/index.ts
@@ -1,136 +1,136 @@
 // This is a sample supporting file mustache template.
 
-export * from './types'
+export * from './v1/types'
 
-export { default as addPrincipalToGroup } from './AddPrincipalToGroup'
-
-
-// end of operations block
-export { default as addRoleToGroup } from './AddRoleToGroup'
+export { default as addPrincipalToGroup } from './v1/AddPrincipalToGroup'
 
 
 // end of operations block
-export { default as createCrossAccountRequests } from './CreateCrossAccountRequests'
+export { default as addRoleToGroup } from './v1/AddRoleToGroup'
 
 
 // end of operations block
-export { default as createGroup } from './CreateGroup'
+export { default as createCrossAccountRequests } from './v1/CreateCrossAccountRequests'
 
 
 // end of operations block
-export { default as createPolicies } from './CreatePolicies'
+export { default as createGroup } from './v1/CreateGroup'
 
 
 // end of operations block
-export { default as createRole } from './CreateRole'
+export { default as createPolicies } from './v1/CreatePolicies'
 
 
 // end of operations block
-export { default as deleteGroup } from './DeleteGroup'
+export { default as createRole } from './v1/CreateRole'
 
 
 // end of operations block
-export { default as deletePolicy } from './DeletePolicy'
+export { default as deleteGroup } from './v1/DeleteGroup'
 
 
 // end of operations block
-export { default as deletePrincipalFromGroup } from './DeletePrincipalFromGroup'
+export { default as deletePolicy } from './v1/DeletePolicy'
 
 
 // end of operations block
-export { default as deleteRole } from './DeleteRole'
+export { default as deletePrincipalFromGroup } from './v1/DeletePrincipalFromGroup'
 
 
 // end of operations block
-export { default as deleteRoleFromGroup } from './DeleteRoleFromGroup'
+export { default as deleteRole } from './v1/DeleteRole'
 
 
 // end of operations block
-export { default as getCrossAccountRequest } from './GetCrossAccountRequest'
+export { default as deleteRoleFromGroup } from './v1/DeleteRoleFromGroup'
 
 
 // end of operations block
-export { default as getGroup } from './GetGroup'
+export { default as getCrossAccountRequest } from './v1/GetCrossAccountRequest'
 
 
 // end of operations block
-export { default as getPolicy } from './GetPolicy'
+export { default as getGroup } from './v1/GetGroup'
 
 
 // end of operations block
-export { default as getPrincipalAccess } from './GetPrincipalAccess'
+export { default as getPolicy } from './v1/GetPolicy'
 
 
 // end of operations block
-export { default as getPrincipalsFromGroup } from './GetPrincipalsFromGroup'
+export { default as getPrincipalAccess } from './v1/GetPrincipalAccess'
 
 
 // end of operations block
-export { default as getRole } from './GetRole'
+export { default as getPrincipalsFromGroup } from './v1/GetPrincipalsFromGroup'
 
 
 // end of operations block
-export { default as getRoleAccess } from './GetRoleAccess'
+export { default as getRole } from './v1/GetRole'
 
 
 // end of operations block
-export { default as getStatus } from './GetStatus'
+export { default as getRoleAccess } from './v1/GetRoleAccess'
 
 
 // end of operations block
-export { default as listCrossAccountRequests } from './ListCrossAccountRequests'
+export { default as getStatus } from './v1/GetStatus'
 
 
 // end of operations block
-export { default as listGroups } from './ListGroups'
+export { default as listCrossAccountRequests } from './v1/ListCrossAccountRequests'
 
 
 // end of operations block
-export { default as listPermissionOptions } from './ListPermissionOptions'
+export { default as listGroups } from './v1/ListGroups'
 
 
 // end of operations block
-export { default as listPermissions } from './ListPermissions'
+export { default as listPermissionOptions } from './v1/ListPermissionOptions'
 
 
 // end of operations block
-export { default as listPolicies } from './ListPolicies'
+export { default as listPermissions } from './v1/ListPermissions'
 
 
 // end of operations block
-export { default as listPrincipals } from './ListPrincipals'
+export { default as listPolicies } from './v1/ListPolicies'
 
 
 // end of operations block
-export { default as listRoles } from './ListRoles'
+export { default as listPrincipals } from './v1/ListPrincipals'
 
 
 // end of operations block
-export { default as listRolesForGroup } from './ListRolesForGroup'
+export { default as listRoles } from './v1/ListRoles'
 
 
 // end of operations block
-export { default as patchCrossAccountRequest } from './PatchCrossAccountRequest'
+export { default as listRolesForGroup } from './v1/ListRolesForGroup'
 
 
 // end of operations block
-export { default as patchRole } from './PatchRole'
+export { default as patchCrossAccountRequest } from './v1/PatchCrossAccountRequest'
 
 
 // end of operations block
-export { default as putCrossAccountRequest } from './PutCrossAccountRequest'
+export { default as patchRole } from './v1/PatchRole'
 
 
 // end of operations block
-export { default as updateGroup } from './UpdateGroup'
+export { default as putCrossAccountRequest } from './v1/PutCrossAccountRequest'
 
 
 // end of operations block
-export { default as updatePolicy } from './UpdatePolicy'
+export { default as updateGroup } from './v1/UpdateGroup'
 
 
 // end of operations block
-export { default as updateRole } from './UpdateRole'
+export { default as updatePolicy } from './v1/UpdatePolicy'
+
+
+// end of operations block
+export { default as updateRole } from './v1/UpdateRole'
 
 
 // end of operations block
