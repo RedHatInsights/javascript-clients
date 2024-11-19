93f681689d220f0d11901193c2f09b61402f2a01
Bryan Florkiewicz <bflorkie@redhat.com>
2024-11-19 11:58:22 -0500

    feat: create V2 spec of integrations client RHCLOUD-36225

---
+++ packages/integrations/v2/index.ts
@@ -0,0 +1,30 @@
+export { default as endpointResourceV2AddEventTypeToEndpoint, EndpointResourceV2AddEventTypeToEndpointReturnType } from './EndpointResourceV2AddEventTypeToEndpoint'
+
+export { default as endpointResourceV2CreateEndpoint, EndpointResourceV2CreateEndpointReturnType } from './EndpointResourceV2CreateEndpoint'
+
+export { default as endpointResourceV2DeleteEndpoint, EndpointResourceV2DeleteEndpointReturnType } from './EndpointResourceV2DeleteEndpoint'
+
+export { default as endpointResourceV2DeleteEventTypeFromEndpoint, EndpointResourceV2DeleteEventTypeFromEndpointReturnType } from './EndpointResourceV2DeleteEventTypeFromEndpoint'
+
+export { default as endpointResourceV2DisableEndpoint, EndpointResourceV2DisableEndpointReturnType } from './EndpointResourceV2DisableEndpoint'
+
+export { default as endpointResourceV2EnableEndpoint, EndpointResourceV2EnableEndpointReturnType } from './EndpointResourceV2EnableEndpoint'
+
+export { default as endpointResourceV2GetDetailedEndpointHistory, EndpointResourceV2GetDetailedEndpointHistoryReturnType } from './EndpointResourceV2GetDetailedEndpointHistory'
+
+export { default as endpointResourceV2GetEndpoint, EndpointResourceV2GetEndpointReturnType } from './EndpointResourceV2GetEndpoint'
+
+export { default as endpointResourceV2GetEndpointHistory, EndpointResourceV2GetEndpointHistoryReturnType } from './EndpointResourceV2GetEndpointHistory'
+
+export { default as endpointResourceV2GetEndpoints, EndpointResourceV2GetEndpointsReturnType } from './EndpointResourceV2GetEndpoints'
+
+export { default as endpointResourceV2GetOrCreateDrawerSubscriptionEndpoint, EndpointResourceV2GetOrCreateDrawerSubscriptionEndpointReturnType } from './EndpointResourceV2GetOrCreateDrawerSubscriptionEndpoint'
+
+export { default as endpointResourceV2GetOrCreateEmailSubscriptionEndpoint, EndpointResourceV2GetOrCreateEmailSubscriptionEndpointReturnType } from './EndpointResourceV2GetOrCreateEmailSubscriptionEndpoint'
+
+export { default as endpointResourceV2TestEndpoint, EndpointResourceV2TestEndpointReturnType } from './EndpointResourceV2TestEndpoint'
+
+export { default as endpointResourceV2UpdateEndpoint, EndpointResourceV2UpdateEndpointReturnType } from './EndpointResourceV2UpdateEndpoint'
+
+export { default as endpointResourceV2UpdateEventTypesLinkedToEndpoint, EndpointResourceV2UpdateEventTypesLinkedToEndpointReturnType } from './EndpointResourceV2UpdateEventTypesLinkedToEndpoint'
+
