44c65420c4749731fb0f551c3ea18ac7c349d206
Austin Pinkerton <apinkert@redhat.com>
2024-11-19 13:38:01 -0500

    chore: convert policies-client to new generator

---
+++ packages/policies/DeletePoliciesById/index.ts
@@ -0,0 +1,58 @@
+// @ts-ignore
+import type { AxiosPromise, AxiosInstance, AxiosRequestConfig, Method } from 'axios';
+// @ts-ignore
+import { COLLECTION_FORMATS, RequiredError, AuthTypeEnum, DUMMY_BASE_URL, assertParamExists, setApiKeyToObject, setBasicAuthToObject, setBearerAuthToObject, setOAuthToObject, setSearchParams, serializeDataIfNeeded, toPathString, createRequestFunction } from '@redhat-cloud-services/javascript-clients-shared/dist/common';
+import type { RequestArgs } from '@redhat-cloud-services/javascript-clients-shared/dist/common';
+// @ts-ignore
+import { BaseAPI } from '@redhat-cloud-services/javascript-clients-shared/dist/base';
+import { Configuration } from '@redhat-cloud-services/javascript-clients-shared/dist/configuration';
+
+// @ts-ignore
+import type {  } from '../types';
+
+
+export type DeletePoliciesByIdParams = {
+  /**
+  * UUID of the policy
+  * @type { string }
+  * @memberof DeletePoliciesByIdApi
+  */
+  id: string,
+  options?: AxiosRequestConfig
+}
+
+export type DeletePoliciesByIdReturnType = AxiosPromise<void>;
+
+const isDeletePoliciesByIdObjectParams = (params: [DeletePoliciesByIdParams] | unknown[]): params is [DeletePoliciesByIdParams] => {
+  return params.length === 1 && Object.prototype.hasOwnProperty.call(params, 'id')
+}
+/**
+*
+* @summary Delete a single policy for a customer by its id
+* @param {DeletePoliciesByIdParams} config with all available params.
+* @param {*} [options] Override http request option.
+* @throws {RequiredError}
+*/
+export const deletePoliciesByIdParamCreator = async (...config: ([DeletePoliciesByIdParams] | [string, AxiosRequestConfig])): Promise<RequestArgs> => {
+    const params = isDeletePoliciesByIdObjectParams(config) ? config[0] : ['id', 'options'].reduce((acc, curr, index) => ({ ...acc, [curr]: config[index] }), {}) as DeletePoliciesByIdParams;
+    const { id, options = {} } = params;
+    const localVarPath = `/policies/{id}`
+        .replace(`{${"id"}}`, encodeURIComponent(String(id)));
+    // use dummy base URL string because the URL constructor only accepts absolute URLs.
+    const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+    const localVarRequestOptions = { method: 'DELETE' as Method, ...options};
+    const localVarHeaderParameter = {} as any;
+    const localVarQueryParameter = {} as any;
+
+
+
+    setSearchParams(localVarUrlObj, localVarQueryParameter);
+    localVarRequestOptions.headers = {...localVarHeaderParameter, ...options.headers};
+
+    return {
+        urlObj: localVarUrlObj,
+        options: localVarRequestOptions,
+    };
+}
+
+export default deletePoliciesByIdParamCreator;
