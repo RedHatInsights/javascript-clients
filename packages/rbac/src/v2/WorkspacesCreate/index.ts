30f57e3712c7c31a873e390fe092b0b10ae1e5db
Charles Mulder <chmulder@redhat.com>
2026-02-09 14:47:31 +0000

    refactor(rbac)!: restructure package

---
+++ packages/rbac/src/v2/WorkspacesCreate/index.ts
@@ -0,0 +1,66 @@
+// @ts-ignore
+import type { AxiosPromise, AxiosInstance, AxiosRequestConfig, Method } from 'axios';
+// @ts-ignore
+import { COLLECTION_FORMATS, RequiredError, AuthTypeEnum, DUMMY_BASE_URL, assertParamExists, setApiKeyToObject, setBasicAuthToObject, setBearerAuthToObject, setOAuthToObject, setSearchParams, serializeDataIfNeeded, toPathString, createRequestFunction } from '@redhat-cloud-services/javascript-clients-shared/dist/common';
+import type { RequestArgs } from '@redhat-cloud-services/javascript-clients-shared/dist/common';
+// @ts-ignore
+import { BaseAPI } from '@redhat-cloud-services/javascript-clients-shared/dist/base';
+import { Configuration } from '@redhat-cloud-services/javascript-clients-shared/dist/configuration';
+
+// @ts-ignore
+import type { ProblemsProblem400, ProblemsProblem403, RoleBindingsList401Response, RoleBindingsList500Response, WorkspacesCreateWorkspaceRequest, WorkspacesCreateWorkspaceResponse } from '../types';
+
+
+export type WorkspacesCreateParams = {
+  /**
+  *
+  * @type { WorkspacesCreateWorkspaceRequest }
+  * @memberof WorkspacesCreateApi
+  */
+  workspacesCreateWorkspaceRequest: WorkspacesCreateWorkspaceRequest,
+  options?: AxiosRequestConfig
+}
+
+export type WorkspacesCreateReturnType = WorkspacesCreateWorkspaceResponse;
+
+const isWorkspacesCreateObjectParams = (params: [WorkspacesCreateParams] | unknown[]): params is [WorkspacesCreateParams] => {
+  const l = params.length === 1
+  if(l && typeof params[0] === 'object' && !Array.isArray(params[0])) {
+    return true && Object.prototype.hasOwnProperty.call(params[0], 'workspacesCreateWorkspaceRequest')
+  }
+  return false
+}
+/**
+* Create workspace in tenant
+* @summary Create workspace in tenant
+* @param {WorkspacesCreateParams} config with all available params.
+* @param {*} [options] Override http request option.
+* @throws {RequiredError}
+*/
+export const workspacesCreateParamCreator = async (sendRequest: BaseAPI["sendRequest"], ...config: ([WorkspacesCreateParams] | [WorkspacesCreateWorkspaceRequest, AxiosRequestConfig])) => {
+    const params = isWorkspacesCreateObjectParams(config) ? config[0] : ['workspacesCreateWorkspaceRequest', 'options'].reduce((acc, curr, index) => ({ ...acc, [curr]: config[index] }), {}) as WorkspacesCreateParams;
+    const { workspacesCreateWorkspaceRequest, options = {} } = params;
+    const localVarPath = `/workspaces/`;
+    // use dummy base URL string because the URL constructor only accepts absolute URLs.
+    const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+    const localVarRequestOptions = { method: 'POST' as Method, ...options};
+    const localVarHeaderParameter = {} as any;
+    const localVarQueryParameter = {} as any;
+
+
+
+    localVarHeaderParameter['Content-Type'] = 'application/json';
+
+    setSearchParams(localVarUrlObj, localVarQueryParameter);
+    localVarRequestOptions.headers = {...localVarHeaderParameter, ...options.headers};
+
+    const args = {
+        urlObj: localVarUrlObj,
+        options: localVarRequestOptions,
+        serializeData: workspacesCreateWorkspaceRequest,
+    };
+
+    return sendRequest<WorkspacesCreateReturnType>(Promise.resolve(args));
+}
+
+export default workspacesCreateParamCreator;
---
+++ packages/rbac/src/v2/WorkspacesUpdate/index.ts
@@ -0,0 +1,73 @@
+// @ts-ignore
+import type { AxiosPromise, AxiosInstance, AxiosRequestConfig, Method } from 'axios';
+// @ts-ignore
+import { COLLECTION_FORMATS, RequiredError, AuthTypeEnum, DUMMY_BASE_URL, assertParamExists, setApiKeyToObject, setBasicAuthToObject, setBearerAuthToObject, setOAuthToObject, setSearchParams, serializeDataIfNeeded, toPathString, createRequestFunction } from '@redhat-cloud-services/javascript-clients-shared/dist/common';
+import type { RequestArgs } from '@redhat-cloud-services/javascript-clients-shared/dist/common';
+// @ts-ignore
+import { BaseAPI } from '@redhat-cloud-services/javascript-clients-shared/dist/base';
+import { Configuration } from '@redhat-cloud-services/javascript-clients-shared/dist/configuration';
+
+// @ts-ignore
+import type { ProblemsProblem403, RoleBindingsList401Response, RoleBindingsList500Response, WorkspacesUpdate400Response, WorkspacesUpdateWorkspaceRequest, WorkspacesUpdateWorkspaceResponse } from '../types';
+
+
+export type WorkspacesUpdateParams = {
+  /**
+  * Unique identification
+  * @type { string }
+  * @memberof WorkspacesUpdateApi
+  */
+  id: string,
+  /**
+  *
+  * @type { WorkspacesUpdateWorkspaceRequest }
+  * @memberof WorkspacesUpdateApi
+  */
+  workspacesUpdateWorkspaceRequest: WorkspacesUpdateWorkspaceRequest,
+  options?: AxiosRequestConfig
+}
+
+export type WorkspacesUpdateReturnType = WorkspacesUpdateWorkspaceResponse;
+
+const isWorkspacesUpdateObjectParams = (params: [WorkspacesUpdateParams] | unknown[]): params is [WorkspacesUpdateParams] => {
+  const l = params.length === 1
+  if(l && typeof params[0] === 'object' && !Array.isArray(params[0])) {
+    return true && Object.prototype.hasOwnProperty.call(params[0], 'id') && Object.prototype.hasOwnProperty.call(params[0], 'workspacesUpdateWorkspaceRequest')
+  }
+  return false
+}
+/**
+* Edit a workspace
+* @summary Edit the workspace name or description
+* @param {WorkspacesUpdateParams} config with all available params.
+* @param {*} [options] Override http request option.
+* @throws {RequiredError}
+*/
+export const workspacesUpdateParamCreator = async (sendRequest: BaseAPI["sendRequest"], ...config: ([WorkspacesUpdateParams] | [string, WorkspacesUpdateWorkspaceRequest, AxiosRequestConfig])) => {
+    const params = isWorkspacesUpdateObjectParams(config) ? config[0] : ['id', 'workspacesUpdateWorkspaceRequest', 'options'].reduce((acc, curr, index) => ({ ...acc, [curr]: config[index] }), {}) as WorkspacesUpdateParams;
+    const { id, workspacesUpdateWorkspaceRequest, options = {} } = params;
+    const localVarPath = `/workspaces/{id}/`
+        .replace(`{${"id"}}`, encodeURIComponent(String(id)));
+    // use dummy base URL string because the URL constructor only accepts absolute URLs.
+    const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+    const localVarRequestOptions = { method: 'PUT' as Method, ...options};
+    const localVarHeaderParameter = {} as any;
+    const localVarQueryParameter = {} as any;
+
+
+
+    localVarHeaderParameter['Content-Type'] = 'application/json';
+
+    setSearchParams(localVarUrlObj, localVarQueryParameter);
+    localVarRequestOptions.headers = {...localVarHeaderParameter, ...options.headers};
+
+    const args = {
+        urlObj: localVarUrlObj,
+        options: localVarRequestOptions,
+        serializeData: workspacesUpdateWorkspaceRequest,
+    };
+
+    return sendRequest<WorkspacesUpdateReturnType>(Promise.resolve(args));
+}
+
+export default workspacesUpdateParamCreator;
