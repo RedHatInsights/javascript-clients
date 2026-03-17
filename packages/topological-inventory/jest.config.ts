73e79c2751245fc9454168a95f04175e10308725
Olha Tomylko <otomylko@redhat.com>
2026-03-17 12:54:15 +0100

    build(topological-inventory): restructure to generate into src directory
      Align topological-inventory package with notifications/integrations build
      pattern: generate TypeScript into src/, add clean-generate and
      clean-build targets, update tsconfig to target ES2021 for ESM, publish
      from dist directory.

--- packages/topological-inventory/jest.config.ts
+++ packages/topological-inventory/jest.config.ts
@@ -7,4 +7,5 @@ export default {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/topological-inventory',
+  testPathIgnorePatterns: ['/node_modules/','/tests/integration/'],
 };
